a823c76cf853ea7b76edfba7f31d418321216364
znzazy <112530266+znzazy@users.noreply.github.com>
2022-08-31 05:13:32 +0200

    Update ubuntu1804googleshell.sh

--- ubuntu1804googleshell.sh
+++ ubuntu1804googleshell.sh
@@ -4,6 +4,6 @@ stty susp undef
 
 clear
 echo "Installing Ubuntu 18.04 Environment  (1-2 minutes)...."
-sudo docker run --name ubuntu18.04 --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 accetto/ubuntu-vnc-xfce 
+sudo docker run --name ubuntu18.04 --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 vital987/vubuntu 
 echo "All done! Please click Web Preview > Preview On Port 8080 to access your VM!"
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
