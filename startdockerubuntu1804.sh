d9f8e80d5d17fa54742fd256c1d721f0db707ab8
znzazy <112530266+znzazy@users.noreply.github.com>
2022-08-31 04:47:04 +0200

    Update startdockerubuntu1804.sh

--- startdockerubuntu1804.sh
+++ startdockerubuntu1804.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -skLo ubuntu1804 https://raw.githubusercontent.com/kmille36/DockerUbuntu18CrossOver/main/ubuntu1804googleshell.sh 
+curl -skLo ubuntu1804 https://raw.githubusercontent.com/znzazy/Ubuntu/main/ubuntu1804googleshell.sh 
 chmod +x ubuntu1804
 bash ubuntu1804
